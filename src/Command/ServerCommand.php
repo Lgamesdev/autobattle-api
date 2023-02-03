8ae0952f8b4d4afb6146271fb23a5ae1d7366b1d
Lgamesdev <louisgamesdev@gmail.com>
2023-02-03 17:30:39 +0100

    websocket token verification

--- src/Command/ServerCommand.php
+++ src/Command/ServerCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Command;
 
 use App\Websocket\Chat;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Ratchet\Http\HttpServer;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
@@ -13,10 +14,20 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 #[AsCommand(name: 'run:websocket-server')]
 class ServerCommand extends Command
 {
+    private TokenStorageInterface $tokenStorageInterface;
+    private JWTTokenManagerInterface $jwtManager;
+
+    public function __construct(TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager) {
+        parent::__construct();
+        $this->tokenStorageInterface = $tokenStorageInterface;
+        $this->jwtManager = $jwtManager;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $port = 5120;
@@ -24,7 +35,7 @@ class ServerCommand extends Command
         $server = IoServer::factory(
             new HttpServer(
                 new WsServer(
-                    new Chat($output)
+                    new Chat($output, $this->tokenStorageInterface, $this->jwtManager)
                 )
             ),
             $port
--- src/Websocket/Chat.php
+++ src/Websocket/Chat.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\Websocket;
 
 use Exception;
+use FTP\Connection;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Ratchet\ConnectionInterface;
 use Ratchet\MessageComponentInterface;
 use SplObjectStorage;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class Chat implements MessageComponentInterface
 {
@@ -16,24 +19,36 @@ class Chat implements MessageComponentInterface
     private $botName = 'ChatBot';
     private $defaultChannel = 'general';
 
+    private TokenStorageInterface $tokenStorageInterface;
+    private JWTTokenManagerInterface $jwtManager;
     protected SplObjectStorage $connections;
     private OutputInterface $output;
 
-    public function __construct(OutputInterface $output) {
+    public function __construct(OutputInterface $output, TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager) {
         $this->connections = new SplObjectStorage;
         $this->output = $output;
+        $this->tokenStorageInterface = $tokenStorageInterface;
+        $this->jwtManager = $jwtManager;
     }
 
     public function onOpen(ConnectionInterface $conn) {
-        $this->output->writeln('new connection : #' . $conn->resourceId);
-        if(array_key_exists($conn->resourceId, $this->users)) {
-            $this->users[$conn->resourceId] = [
-                'connection' => $conn,
-                'user' => '',
-                'channels' => []
-            ];
-            $this->connections->attach($conn);
+        $username = $this->getUsername($conn);
+
+        $this->output->writeln('new connection from : ' . $username . ' #' . $conn->resourceId);
+
+        if(array_key_exists($username, $this->users)) {
+            $this->connections->detach($this->users[$username]['connection']);
+            $this->users[$username]['connection']->close();
+            unset($this->users[$username]);
         }
+
+        $this->users[$username] = [
+            'connection' => $conn,
+            'user' => $username,
+            'channels' => []
+        ];
+
+        $this->connections->attach($conn);
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
@@ -66,7 +81,7 @@ class Chat implements MessageComponentInterface
 
     private function subscribeToChannel(ConnectionInterface $conn, $channel, $user)
     {
-        $this->users[$conn->resourceId]['channels'][$channel] = $channel;
+        $this->users[$this->getUsername($conn)]['channels'][$channel] = $channel;
         $this->sendMessageToChannel(
             $conn,
             $channel,
@@ -77,8 +92,8 @@ class Chat implements MessageComponentInterface
 
     private function unsubscribeFromChannel(ConnectionInterface $conn, $channel, $user)
     {
-        if (array_key_exists($channel, $this->users[$conn->resourceId]['channels'])) {
-            unset($this->users[$conn->resourceId]['channels']);
+        if (array_key_exists($channel, $this->users[$this->getUsername($conn)]['channels'])) {
+            unset($this->users[$this->getUsername($conn)]['channels']);
         }
         $this->sendMessageToChannel(
             $conn,
@@ -90,10 +105,10 @@ class Chat implements MessageComponentInterface
 
     private function sendMessageToChannel(ConnectionInterface $conn, $channel, $user, $message): bool
     {
-        if (!isset($this->users[$conn->resourceId]['channels'][$channel])) {
+        if (!isset($this->users[$this->getUsername($conn)]['channels'][$channel])) {
             return false;
         }
-        foreach ($this->users as $connectionId => $userConnection) {
+        foreach ($this->users as $username => $userConnection) {
             if (array_key_exists($channel, $userConnection['channels'])) {
                 $userConnection['connection']->send(json_encode([
                     'action' => 'message',
@@ -108,13 +123,19 @@ class Chat implements MessageComponentInterface
 
     public function onClose(ConnectionInterface $conn) {
         $this->output->writeln('connection closed');
-        unset($this->users[$conn->resourceId]);
+        unset($this->users[$this->getUsername($conn)]);
         $this->connections->detach($conn);
     }
 
     public function onError(ConnectionInterface $conn, Exception $e) {
-        $this->output->writeln('new error');
+        $this->output->writeln('new error : ' . $e);
         $this->connections->detach($conn);
         $conn->close();
+    }
+
+    public function getUsername(ConnectionInterface $conn) : string
+    {
+        $headers = $conn->httpRequest->getHeaders();
+        return $this->jwtManager->parse($headers['Authorization'][0])['username'];
     }
 }
